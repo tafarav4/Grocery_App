0a110ebe449b22beef782afe24b954ccd5e7bf33
tafarav4 <tafarav@stanford.edu>
2024-09-19 11:33:58 -0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 4444;
 
-export const mongoDBURL = 'mongodb+srv://tafara:@cluster0.8k63tty.mongodb.net/groceries-collection?retryWrites=true&w=majority&appName=Cluster0';
+export const mongoDBURL = 'mongodb+srv:_________';
